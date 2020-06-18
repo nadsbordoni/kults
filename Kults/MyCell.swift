a16ab9d7f02e1959aba7c097583560f85b4eae06
Victor Vieira <avieira.victor@gmail.com>
2020-06-18 12:55:02 -0300

    CollectionView pronto

---
+++ Kults/MyCell.swift
@@ -0,0 +1,19 @@
+//
+//  MyCell.swift
+//  Kults
+//
+//  Created by Victor Vieira on 18/06/20.
+//  Copyright © 2020 Nádia Bordoni. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyCell : UICollectionViewCell{
+    
+    
+    @IBOutlet weak var animationImage: UIImageView!
+    
+}
+
+
--- Kults/ViewController.swift
+++ Kults/ViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let layout = myCollectionView.collectionViewLayout as? UICollectionViewFlowLayout
+        layout?.minimumLineSpacing = 42
+        layout?.collectionView?.backgroundColor = nil
     }
 
-
+    let animations = [UIImage(named: "titleAnimationOne"),UIImage(named: "titleAnimationOne"),UIImage(named: "titleAnimationOne"),UIImage(named: "titleAnimationOne"),UIImage(named: "titleAnimationOne"),UIImage(named: "titleAnimationOne"),UIImage(named: "titleAnimationOne")]
+    
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return animations.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "my cell", for: indexPath as IndexPath) as! MyCell
+        
+        cell.animationImage.image = animations[indexPath.item]
+        cell.layer.cornerRadius = 15
+        cell.layer.borderColor = UIColor.white.cgColor
+        cell.layer.borderWidth = 3
+        return cell
+        
+    }
+    
 }
 
